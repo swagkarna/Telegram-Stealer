59edf037ccac31b5a73ef308e36c7f3a60eed771
swagkarna <46685308+swagkarna@users.noreply.github.com>
2023-03-26 08:08:30 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,11 +1,10 @@
 @echo off
-color 0a 
-
+color 0a
+echo.
+echo.
 type run.txt
-echo. 
-echo. 
-@echo off
-echo 1. Install Required Module 
+
+echo 1. Install Required Module
 echo 2. Convert ps1 to exe
 
 set /p input1="Enter your choice: "
@@ -13,7 +12,7 @@ set /p input1="Enter your choice: "
 if "%input1%"=="1" (
   echo Installing Required Modules... Please Wait...
   REM Install ps2exe module from PowerShell Gallery
-  powershell -ExecutionPolicy Bypass -Command "Install-Module -Name ps2exe -Scope CurrentUser"
+  call powershell -ExecutionPolicy Bypass -Command "Install-Module -Name ps2exe -Scope CurrentUser"
 
   REM Check if installation was successful
   if %errorlevel% equ 0 (
@@ -22,10 +21,10 @@ if "%input1%"=="1" (
     echo Installation failed.
   )
 ) else if "%input1%"=="2" (
-  set /p input_file="Enter input_file name: "
-  set /p output_file="Enter output_file name: "
+  set /p input_file="Enter input_file name (including .ps1 extension): "
+  set /p output_file="Enter output_file name (including .exe extension): "
   REM Convert the PowerShell script to executable using ps2exe
-  powershell -ExecutionPolicy Bypass -Command "Import-Module ps2exe; ps2exe.ps1 -inputFile '%input_file%' -outputFile '%output_file%' -lcid 1033 -noConsole -ErrorAction SilentlyContinue"
+  call powershell -ExecutionPolicy Bypass -Command "Import-Module ps2exe; ps2exe.ps1 -inputFile '%input_file%' -outputFile '%output_file%' -lcid 1033 -noConsole -ErrorAction SilentlyContinue"
 
   REM Check if conversion was successful
   if %errorlevel% equ 0 (
@@ -36,4 +35,3 @@ if "%input1%"=="1" (
 ) else (
   echo Invalid choice.
 )
-
